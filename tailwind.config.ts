efad7b87dcc14e2b324636cdb1ed039b6ed66bb2
PREM HAGARAGI <premversace@gmail.com>
2025-07-30 07:32:12 +0000

    use Inter font across the website

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Poppins', 'Roboto', 'sans-serif'],
-        headline: ['Poppins', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Inter', 'sans-serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
